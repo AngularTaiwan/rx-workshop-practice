67e68a5ad475df0ca4bcf4826d8ccf471e6c126a
Kevin Yang <chgc_kevin@hotmail.com>
2018-01-14 22:42:38 +0800

    refactor code in reactive programming style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { MatCheckboxChange } from '@angular/material';
 import { Observable } from 'rxjs/Observable';
+import { tap, takeUntil, map } from 'rxjs/operators';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  registerForm: FormGroup;
+export class AppComponent implements OnInit, OnDestroy {
 
-  regions$: Observable<any>;
+  registerForm: FormGroup = new FormGroup({
+    name: new FormControl('', Validators.required),
+    subscription: new FormGroup({
+      checkboxAll: new FormControl(),
+      subscribeRxWorkshop: new FormControl(true),
+      subscribeAngularMaterial: new FormControl(true),
+      subscribeAngularTutorial: new FormControl(true),
+      subscribeAngularMaster: new FormControl(false)
+    }),
+    city: new FormControl(),
+    area: new FormControl()
+  });
+  destroy$ = new Subject();
+
+  regions$: Observable<any> = this.httpClient.get<any>('/assets/region.json');
 
   newsletterList = [
     { id: 'subscribeRxWorkshop', name: 'Rx Workshop' },
@@ -21,50 +36,75 @@ export class AppComponent implements OnInit {
     { id: 'subscribeAngularMaster', name: 'Angular大師養成班' }
   ];
 
-  get checkAll() {
-    const subscriptionCount = this._getSubscriptionCount();
-    return subscriptionCount === this.newsletterList.length;
-  }
+  indeterminateSubscription: boolean = false;
 
-  get indeterminateSubscription() {
-    const subscriptionCount = this._getSubscriptionCount();
-    return subscriptionCount !== 0 && subscriptionCount !== this.newsletterList.length;
+  ngOnDestroy() {
+    this.destroy$.next('');
+    this.destroy$.complete();
   }
-
-  constructor(private httpClient: HttpClient) {
-    this.registerForm = new FormGroup({
-      name: new FormControl('', Validators.required),
-      subscription: new FormGroup({
-        subscribeRxWorkshop: new FormControl(true),
-        subscribeAngularMaterial: new FormControl(true),
-        subscribeAngularTutorial: new FormControl(true),
-        subscribeAngularMaster: new FormControl(false)
-      }),
-      city: new FormControl(),
-      area: new FormControl()
-    });
-  }
-
   ngOnInit() {
-    this.regions$ = this.httpClient.get<any>('/assets/region.json');
+    this.registerForm.get('subscription.checkboxAll').valueChanges.pipe(
+      tap(value => {
+        this.newsletterList.forEach(newsletter => {
+          this.registerForm.get(`subscription.${newsletter.id}`).setValue(value);
+        });
+      }),
+      takeUntil(this.destroy$)
+    ).subscribe();
+
+    this.registerForm.get('subscription').valueChanges.pipe(
+      map(values => Object.keys(values)),
+      map(arKeys => arKeys.filter(key => key !== 'checkboxAll')),
+      map(keys => {
+        return keys.reduce((subscriptionCount: number, contorlKey: string) => {
+          subscriptionCount += (this.registerForm.get('subscription') as FormGroup).controls[contorlKey].value ? 1 : 0;
+          return subscriptionCount;
+        }, 0);
+      }),
+      tap(subscriptionCount => {
+        let checked = false;
+        this.indeterminateSubscription = subscriptionCount !== 0 && subscriptionCount !== this.newsletterList.length;
+        if (subscriptionCount === this.newsletterList.length) {
+          checked = true;
+        }
+        this.registerForm.get('subscription.checkboxAll').patchValue(checked, { emitEvent: false });
+      }),
+      takeUntil(this.destroy$)
+    ).subscribe();
+
+    this.registerForm.reset({
+      subscription: {
+        subscribeRxWorkshop: true
+      }
+    });
   }
 
-  private _getSubscriptionCount() {
-    let subscriptionCount = 0;
-    const subscriptionControls = (this.registerForm.controls.subscription as FormGroup).controls;
-    Object.keys(subscriptionControls).forEach(contorlKey => {
-      subscriptionCount += subscriptionControls[contorlKey].value ? 1 : 0;
-    });
-    return subscriptionCount;
-  }
+  // get indeterminateSubscription() {
+  //   const subscriptionCount = this._getSubscriptionCount();
+  //   return subscriptionCount !== 0 && subscriptionCount !== this.newsletterList.length;
+  // }
 
-  subscribeAllChange($event: MatCheckboxChange) {
-    this.newsletterList.forEach(newsletter => {
-      this.registerForm.get(`subscription.${newsletter.id}`).setValue($event.checked);
-    });
-  }
+  // private _getSubscriptionCount() {
+  //   const subscriptionControls = (this.registerForm.get('subscription') as FormGroup).controls;
+
+  //   return Object.keys(subscriptionControls)
+  //     .filter(key => key !== 'checkboxAll').reduce((subscriptionCount: number, contorlKey: string) => {
+  //       subscriptionCount += subscriptionControls[contorlKey].value ? 1 : 0;
+  //       return subscriptionCount;
+  //     }, 0);
+  // }
+
+  // subscribeAllChange($event: MatCheckboxChange) {
+  //   this.newsletterList.forEach(newsletter => {
+  //     this.registerForm.get(`subscription.${newsletter.id}`).setValue($event.checked);
+  //   });
+  // }
 
   submit() {
     console.log(this.registerForm.value);
   }
+
+  constructor(private httpClient: HttpClient) {
+  }
+
 }
